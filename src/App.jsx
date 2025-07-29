866388cc03b1c0e7570ac834395a447f8f59e321
alokraj10 <alokrajrnc629@gmail.com>
2025-07-29 12:27:43 +0530

    Updated H1 title

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>Hi, This is Git Action Experiment</h1>
+      <h1>Hi, This is Git Action Experiment by ALOK RAJ (2300033574)</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
